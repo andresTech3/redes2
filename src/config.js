8de7ec7b778ab6a765b497ea6ee9fe34d09410f8
andres felipe <andresnarvaez304@gmail.com>
2024-09-28 08:22:22 -0500

    modificando el archivo config

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_PORT = process.env.DB_PORT || 3306;
 const DB_USER = process.env.DB_USER || "root";
-const DB_PASS = process.env.DB_PASS || "12345";
+const DB_PASS = process.env.DB_PASS || 12345;
 const DATABASE = process.env.DATABASE || "redes2";
 
 export { PORT, DB_HOST, DB_PORT, DB_USER, DB_PASS, DATABASE };
